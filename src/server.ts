c09646bf12c55f98ffb26927ef874e40b51c9034
Matheussmaced <mateusbyakuya3@gmail.com>
2024-01-16 12:02:16 -0300

    refact: separating app from server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import fastify from 'fastify'
-
-const app = fastify()
-
-app.get('/hello', () => {
-  return 'Hello world!'
-})
+import { app } from './app'
 
 app
   .listen({
+    host: '0.0.0.0',
     port: 3333,
   })
   .then(() => {
