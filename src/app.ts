087c03a1042cf8dd90e5444ffc5b558b9b227677
Matheussmaced <mateusbyakuya3@gmail.com>
2024-01-17 20:12:49 -0300

    feat: configuring prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.users.create({
+  data: {
+    email: 'mateusbyakuya3@gmail.com',
+    password: '123',
+  },
+})
 
 app.get('/hello', () => {
   return 'Hello world!'
